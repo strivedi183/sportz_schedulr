1e31c15cfe3add7af6e36c236422ba9119912f7e
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-09-20 11:04:47 -0400

    Initial rails 4.0 set up with postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SportzSchedulr::Application.config.secret_key_base = '5be4eb5dad516cbaf607bc39c936ba836dec38c68a9a97d30e34cec56f4175647da619b9f2fef6020b8ffc321128f5f7706fa8f33ac3cc12a02491c1cdde4510'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SportzSchedulr::Application.config.session_store :cookie_store, key: '_sportz_schedulr_session'
