dd562ad94036f24a46d0825ff3b191c4c1f615f9
Brian Yingling <brian_yingling@yahoo.com>
2013-10-27 13:26:16 -0400

    init commit to feature branchdev

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,6 +1,14 @@
 class MeetupsController < ApplicationController
   def new
+    redirect_to root_path and return unless @auth.id == params[:user_id].to_i
+
+    @user   = User.find params[:user_id]
+    @event  = Event.find params[:event_id]
+    @google_client = GooglePlaces::Client.new ENV['GOOGLE_API_KEY']
+    @spots = @google_client.spots(@event.venue.lat, @event.venue.lng, :types => 'bar', :radius => 5000)
+    render 'events/show'
   end
+
   def index
   end
 end
